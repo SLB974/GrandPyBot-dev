3f9fd1db0e6f2267b125aa8f3a3e5ac153329830
Stéphane LE BOURGEOIS <52003471+SLB974@users.noreply.github.com>
2021-11-09 08:39:00 +0400

    configuring unittest suite in tests_unittest.py

---
+++ app/core/tests/tests_unittest.py
@@ -0,0 +1,18 @@
+import unittest
+
+from . import test_grandpy as grandpy
+from . import test_parser as parser
+from . import test_google as google
+from . import test_wiki as wiki
+from . import test_fetcher as fetcher
+
+loader = unittest.TestLoader()
+suite = unittest.TestSuite()
+suite.addTests(loader.loadTestsFromModule(grandpy))
+suite.addTests(loader.loadTestsFromModule(parser))
+suite.addTests(loader.loadTestsFromModule(google))
+suite.addTests(loader.loadTestsFromModule(wiki))
+suite.addTests(loader.loadTestsFromModule(fetcher))
+
+runner = unittest.TextTestRunner(verbosity=3)
+resulte = runner.run(suite)
