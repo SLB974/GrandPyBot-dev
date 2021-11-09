a4d6d3cf5d3a8a4e1bc1b7ded597a8f7e79cee7d
Stéphane LE BOURGEOIS <52003471+SLB974@users.noreply.github.com>
2021-11-09 07:20:52 +0400

    refactoring test_parser.py with unittest

--- app/core/tests/test_parser.py
+++ app/core/tests/test_parser.py
@@ -1,20 +1,11 @@
-import pytest
+import unittest
 from app.core.parser.parser import Parser
 
 
-question = "Bonjour GrandPy, tu connais la tour eiffel ?"
+class ParserTestCase(unittest.TestCase):
 
+    parser = Parser("Bonjour GrandPy, tu connais la tour eiffel ?")
+    expected = ["tour", "eiffel"]
 
-@pytest.fixture
-def parser():
-    return Parser(question)
-
-
-def test_constructor():
-    p = Parser(question)
-    assert isinstance(p, Parser)
-    assert p.user_query == question
-
-
-def test_clean_string(parser):
-    assert parser.clean_string() == ["tour", "eiffel"]
+    def test_clean_string(self):
+        self.assertEqual(self.parser.clean_string(), self.expected)
