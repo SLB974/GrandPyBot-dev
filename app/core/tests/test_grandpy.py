e87d71a263876f8b918d0606a8da73346193ef0a
Stéphane LE BOURGEOIS <52003471+SLB974@users.noreply.github.com>
2021-11-09 07:03:42 +0400

    refactoring test_grandpy.py with unittest

--- app/core/tests/test_grandpy.py
+++ app/core/tests/test_grandpy.py
@@ -1,38 +1,27 @@
-import pytest
+import unittest
 from app.core.grandpy.grandpy import GrandPy
 from app.core.grandpy.utils import grandpy_response, grandpy_none, grandpy_catch_phrase
 from app.core.grandpy.utils import grandpy_catch_wiki, grandpy_error, grandpy_next
 
 
-@pytest.fixture
-def grandpy():
-    return GrandPy()
+class GrandPyTestCase(unittest.TestCase):
 
+    grandpy = GrandPy()
 
-def test_constructor():
-    g = GrandPy()
-    assert isinstance(g, GrandPy)
+    def test_empty_response(self):
+        self.assertIn(self.grandpy.empty_response(), grandpy_none)
 
+    def test_catch_phrase(self):
+        self.assertIn(self.grandpy.catch_phrase(), grandpy_catch_phrase)
 
-def test_empty_response(grandpy):
-    assert grandpy.empty_response() in grandpy_none
+    def test_error_response(self):
+        self.assertIn(self.grandpy.error_response(), grandpy_error)
 
+    def test_anecdote(self):
+        self.assertIn(self.grandpy.anecdote(), grandpy_catch_wiki)
 
-def test_catch_phrase(grandpy):
-    assert grandpy.catch_phrase() in grandpy_catch_phrase
+    def test_positive_response(self):
+        self.assertIn(self.grandpy.positive_response(), grandpy_response)
 
-
-def test_error_response(grandpy):
-    assert grandpy.error_response() in grandpy_error
-
-
-def test_anecdote(grandpy):
-    assert grandpy.anecdote() in grandpy_catch_wiki
-
-
-def test_positive_response(grandpy):
-    assert grandpy.positive_response() in grandpy_response
-
-
-def test_next_query(grandpy):
-    assert grandpy.next_query() in grandpy_next
+    def test_next_query(self):
+        self.assertIn(self.grandpy.next_query(), grandpy_next)
