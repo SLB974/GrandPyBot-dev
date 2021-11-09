d95afc5f693b9392c7a9ed79b70ff5b0fbbdb3cf
Stéphane LE BOURGEOIS <52003471+SLB974@users.noreply.github.com>
2021-11-09 13:48:05 +0400

    refactoring fetch.py with unittest

--- app/core/tests/test_fetcher.py
+++ app/core/tests/test_fetcher.py
@@ -1,45 +1,39 @@
-from unittest.mock import MagicMock, patch
-import pytest
+from unittest.mock import patch, MagicMock
+from unittest import TestCase
 
 from app.core.fetcher.fetch import GlobalFetcher
 
-query = "Bonjour GrandPy, tu connais la tour eiffel ?"
+user_query = "Bonjour GrandPy, tu connais la tour eiffel ?"
 
 
-@pytest.fixture
-def fetcher():
-    return GlobalFetcher(query)
+def mocked_geocoder_response():
+    """returns geocoder fake response"""
+
+    return {
+        "grandpy_none": None,
+        "grandpy_response": "Voici ce que j'ai trouvé :",
+        "grandpy_catch_wiki": "J'ai vécu là-bas tu sais ?",
+        "grandpy_next_query": "Alors on fait quoi maintenant ?",
+        "map_link": "un lien",
+        "address": "une adresse",
+    }
 
 
-def test_constructor():
-    f = GlobalFetcher(query)
-    assert isinstance(f, GlobalFetcher)
+def mocked_wikiloader_response():
+    """returns wikiloader fake response"""
+
+    return {"anecdote": "Une anecdote", "url": "un url"}
 
 
-def test_get_data_calls_get_geocoder_data(fetcher):
-    fetcher._GlobalFetcher__get_geocoder_data = MagicMock()
-    actual = fetcher.get_data()
-    fetcher._GlobalFetcher__get_geocoder_data.assert_called_once()
+class GlobalFetcherTestCase(TestCase):
 
+    globalfetcher = GlobalFetcher(user_query)
 
-def test_get_data_calls_get_wikiloader_data(fetcher):
-    fetcher._GlobalFetcher__get_wikiloader_data = MagicMock()
-    actual = fetcher.get_data()
-    fetcher._GlobalFetcher__get_wikiloader_data.assert_called_once()
-
-
-# def test_get_data_return_geocoder_and_wikiloader_data(fetcher):
-#     expected = {
-#         "grandpy_none": None,
-#         "grandpy_response": "Pour répondre à ta demande, voici ce que j'ai trouvé : ",
-#         "grandpy_catch_wiki": "Quelle coincidence, je connais très bien vois-tu... Savais-tu que...",
-#         "grandpy_next_query": "Alors, penses-tu pouvoir me soumettre un endroit plus difficile à présent ?",
-#         "map_link": "https://maps.googleapis.com/maps/api/staticmap?center=48.85837009999999,2.2944813&size=300x300&zoom=14&markers=color:red%7C48.85837009999999,2.2944813&key=AIzaSyDkeF5Xu-WxkLQWfA4TBXdcupRDi6KSeRY",
-#         "address": "Voici l'adresse : Champ de Mars, 5 Av. Anatole France, 75007 Paris, France",
-#         "wiki_response": "L'esplanade des Ouvriers-de-la-Tour-Eiffel est une voie située dans le 7e arrondissement de Paris, en France.\n\n\n== Situation et accès ==\nCette place piétonne se trouve sur le Champ-de-Mars, devant la tour Eiffel (côté ouest).\n\n\n== Origine du nom ==\n\n\n== Historique ==\n\n\n== Notes, sources et références ==\n\n\n=== Article connexe ===\nListe des voies du 7e arrondissement de Paris\nEsplanades de Paris Portail de Paris   Portail de la route",
-#         "wiki_link": "https://fr.wikipedia.org/wiki/Esplanade_des_Ouvriers-de-la-Tour-Eiffel",
-#     }
-
-#     actual = fetcher.get_data()
-
-#     assert expected == actual
+    @patch.multiple(
+        "app.core.fetcher.fetch.GlobalFetcher",
+        _GlobalFetcher__get_geocoder_data=MagicMock(return_value=mocked_geocoder_response()),
+        _GlobalFetcher__get_wikiloader_data=MagicMock(return_value=mocked_wikiloader_response()),
+    )
+    def test_get_proper_response_with_usable_query(self):
+        expected = {**mocked_geocoder_response(), **mocked_wikiloader_response()}
+        self.assertEqual(self.globalfetcher.get_data(), expected)
