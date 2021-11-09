e20663cd80453d4e6c2bc300223964cc5f975ff1
Stéphane LE BOURGEOIS <52003471+SLB974@users.noreply.github.com>
2021-11-09 12:58:51 +0400

    refactoring test_wiki.py with unittest

--- app/core/tests/test_wiki.py
+++ app/core/tests/test_wiki.py
@@ -1,5 +1,5 @@
-from unittest.mock import MagicMock
-import pytest
+from unittest.mock import patch
+from unittest import TestCase
 
 from app.core.api_wiki.wiki import WikiLoader
 
@@ -7,37 +7,74 @@ lat = 48.85837009999999
 lng = 2.2944813
 
 
-@pytest.fixture
-def wikiloader():
-    return WikiLoader(lat, lng)
-
-
-def test_constructor():
-    w = WikiLoader(lat, lng)
-    assert isinstance(w, WikiLoader)
-
-
-def test_get_data_return_calls_fetch_wiki(wikiloader):
-    wikiloader._WikiLoader__fetch_wiki = MagicMock()
-    actual = wikiloader.get_data
-    wikiloader._WikiLoader__fetch_wiki.assert_called_once()
-
-
-def test_fetch_title_return(wikiloader):
-    expected = ("Esplanade des Ouvriers-de-la-Tour-Eiffel", 14307238)
-    actual = wikiloader._WikiLoader__fetch_title()
-    assert expected == actual
-
-
-def test_fetch_wiki_return(wikiloader):
-    wikiloader._WikiLoader__fetch_title = MagicMock()
-    wikiloader._WikiLoader__fetch_title.return_value = ("Esplanade des Ouvriers-de-la-Tour-Eiffel", 14307238)
-
-    expected = {
-        "anecdote": "L'esplanade des Ouvriers-de-la-Tour-Eiffel est une voie située dans le 7e arrondissement de Paris, en France.\n\n\n== Situation et accès ==\nCette place piétonne se trouve sur le Champ-de-Mars, devant la tour Eiffel (côté ouest).\n\n\n== Origine du nom ==\n\n\n== Historique ==\n\n\n== Notes, sources et références ==\n\n\n=== Article connexe ===\nListe des voies du 7e arrondissement de Paris\nEsplanades de Paris Portail de Paris   Portail de la route",
-        "url": "https://fr.wikipedia.org/wiki/Esplanade_des_Ouvriers-de-la-Tour-Eiffel",
+def mocked_wiki_title_response_tour_eiffel():
+    """returns wiki response for title and pageid"""
+    return {
+        "batchcomplete": "",
+        "query": {
+            "geosearch": [
+                {
+                    "pageid": 14307238,
+                    "ns": 0,
+                    "title": "Esplanade des Ouvriers-de-la-Tour-Eiffel",
+                    "lat": 48.85834,
+                    "lon": 2.29445,
+                    "dist": 4.1,
+                    "primary": "",
+                }
+            ]
+        },
     }
 
-    actual = wikiloader._WikiLoader__fetch_wiki(("Esplanade des Ouvriers-de-la-Tour-Eiffel", 14307238))
 
-    assert expected == actual
+def mocked_wiki_anecdote_response_tour_eiffel():
+    """returns wiki response for anecdote and url"""
+
+    return {
+        "batchcomplete": "",
+        "query": {
+            "pages": {
+                "14307238": {
+                    "pageid": 14307238,
+                    "ns": 0,
+                    "title": "Esplanade des Ouvriers-de-la-Tour-Eiffel",
+                    "extract": "L'esplanade des Ouvriers-de-la-Tour-Eiffel est une voie situ\u00e9e dans le 7e arrondissement de Paris, en France.\n\n\n== Situation et acc\u00e8s ==\nCette place pi\u00e9tonne se trouve sur le Champ-de-Mars, devant la tour Eiffel (c\u00f4t\u00e9 ouest).\n\n\n== Origine du nom ==\n\n\n== Historique ==\n\n\n== Notes, sources et r\u00e9f\u00e9rences ==\n\n\n=== Article connexe ===\nListe des voies du 7e arrondissement de Paris\nEsplanades de Paris Portail de Paris   Portail de la route",
+                    "contentmodel": "wikitext",
+                    "pagelanguage": "fr",
+                    "pagelanguagehtmlcode": "fr",
+                    "pagelanguagedir": "ltr",
+                    "touched": "2021-10-23T09:22:47Z",
+                    "lastrevid": 187360603,
+                    "length": 1493,
+                    "fullurl": "https://fr.wikipedia.org/wiki/Esplanade_des_Ouvriers-de-la-Tour-Eiffel",
+                    "editurl": "https://fr.wikipedia.org/w/index.php?title=Esplanade_des_Ouvriers-de-la-Tour-Eiffel&action=edit",
+                    "canonicalurl": "https://fr.wikipedia.org/wiki/Esplanade_des_Ouvriers-de-la-Tour-Eiffel",
+                }
+            }
+        },
+    }
+
+
+class WikiLoaderTestCase(TestCase):
+
+    wikiloader = WikiLoader(lat, lng)
+
+    def test_get_title_proper_response_tour_eiffel(self):
+        with patch("app.core.api_wiki.wiki.WikiLoader._WikiLoader__wiki_response") as mocked_method:
+            mocked_method.return_value = mocked_wiki_title_response_tour_eiffel()
+            expected = ("Esplanade des Ouvriers-de-la-Tour-Eiffel", 14307238)
+
+            self.assertEqual(self.wikiloader._WikiLoader__get_title(), expected)
+
+    def test_get_wiki_anecdote_proper_response_tour_eiffel(self):
+        with patch("app.core.api_wiki.wiki.WikiLoader._WikiLoader__wiki_response") as mocked_method:
+            mocked_method.return_value = mocked_wiki_anecdote_response_tour_eiffel()
+            expected = {
+                "anecdote": "L'esplanade des Ouvriers-de-la-Tour-Eiffel est une voie situ\u00e9e dans le 7e arrondissement de Paris, en France.\n\n\n== Situation et acc\u00e8s ==\nCette place pi\u00e9tonne se trouve sur le Champ-de-Mars, devant la tour Eiffel (c\u00f4t\u00e9 ouest).\n\n\n== Origine du nom ==\n\n\n== Historique ==\n\n\n== Notes, sources et r\u00e9f\u00e9rences ==\n\n\n=== Article connexe ===\nListe des voies du 7e arrondissement de Paris\nEsplanades de Paris Portail de Paris   Portail de la route",
+                "url": "https://fr.wikipedia.org/wiki/Esplanade_des_Ouvriers-de-la-Tour-Eiffel",
+            }
+
+            self.assertEqual(
+                self.wikiloader._WikiLoader__get_wiki_anecdote(("Esplanade des Ouvriers-de-la-Tour-Eiffel", 14307238)),
+                expected,
+            )
